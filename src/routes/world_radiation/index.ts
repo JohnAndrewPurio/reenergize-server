f247db8d3d9b5d14dc29d29bf50a1b99ba1a6b6f
JohnAndrewPurio <lyerdestroyer@gmail.com>
2022-05-15 13:37:32 +0800

    Added cors at world_radiation endpoint

--- src/routes/world_radiation/index.ts
+++ src/routes/world_radiation/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import forecasts from "./forecasts"
 import estimatedActuals from "./estimated_actuals"
+import cors from "cors";
 
 const router = Router()
+
+router.use(cors())
 
 router.use("/forecasts", forecasts)
 router.use("/estimated_actuals", estimatedActuals)
